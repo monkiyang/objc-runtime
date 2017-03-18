56c44236dec2b7f0042ea53ad808668fa60c19a0
YangMengQi <597337796@qq.com>
2017-03-18 17:10:04 +0800

    add debug target

---
+++ objc4/objc4-706/debug-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug-objc
+//
+//  Created by YangMengqi on 2017/3/18.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
